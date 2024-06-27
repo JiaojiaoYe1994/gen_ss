168aa04ec610a2681a703aa01031f76518a4ca41
JiaojiaoYe1994 <jiaojiao.ye@tum.de>
2024-06-27 21:03:50 +0000

    add similarity check

--- pascal_id_list.py
+++ pascal_id_list.py
@@ -2,22 +2,44 @@ import os
 import re
 from numpy import random
 # /JPEGImages/2007_000032.jpg /SegmentationClassAug/2007_000032.png
+from similarity_test import calculate_similarity
 
-img_root = "./output/pascal_train"
+similarity_threshold = 0.36 # todo, a little bit smaller than threasold
+base_root = "/mnt/nas/jiaojiao/data/VOC_aug/VOC2012/JPEGImages/"
+img_root = "./output/pascal_train_0618/"
+output = "./train_aug_0618.txt"
 ratio = 1
 datasize = 10582 # Pascal training datasize
 JPGimg_folder = "/JPEGImages/"
 img_list = os.listdir(img_root)
-choice_id = random.choice(len(img_list), datasize*ratio, replace=False) # Choose synthetic image to satisfy real/fake ratio
-print("Length of image in dataset"len(img_list))
+# Choose synthetic image to satisfy real/fake ratio
+choice_id = random.choice(len(img_list), int(datasize*ratio*2.5), replace=False)  
+
+# print("Length of image in dataset", len(img_list))
+# print(choice_id)
 
 pattern = r'_[0-4].png'
 replacement = '_new.png'
-# function1: Generate list of file name for Synthetic dataset
-with open("./train_aug.txt", "w") as f:
+d_sum = 0.
+total_num = int(datasize*ratio)
+cnt = 0 
+# function1: Generate list of file name for Synthetic dataset for train, val
+with open("./train_aug_0618.txt", "w") as f:
     for idx, name in enumerate(img_list):
         if idx in choice_id:
-           name_new = name.replace("_*.png", "_new.png")
-           name_new = re.sub(pattern, replacement, name, flags=re.IGNORECASE)
-           f.write("/JPEGImages/" + name + " /SegmentationClassAug/" + name + '\n')
+#             name_new = name.replace("_*.png", "_new.png")
+            # replace annotation with SAM annotation
+            name_new = re.sub(pattern, replacement, name, flags=re.IGNORECASE)
+            name_base = re.sub(pattern, '.png', name, flags=re.IGNORECASE)
         
+            # similarity filter
+            d = calculate_similarity(base_root+name_base, img_root+name)
+            print(name, d)
+            if d <= similarity_threshold:
+                cnt += 1
+                d_sum += d.item()
+                f.write("/JPEGImages_0618/" + name + " /SegmentationClassAug/" + name_new + '\n')
+                if cnt >= total_num:
+                    break
+                    
+print("Average similarity: ", d_sum / int(datasize*ratio))
---
+++ similarity_test.py
@@ -0,0 +1,87 @@
+# !/usr/bin/env python
+# coding: utf-8
+#
+# Author: Jiaojiao Ye
+# Date:   12 June 2024
+'''The Unreasonable Effectiveness of Deep Features as a Perceptual Metric'''
+import time
+
+import torch
+from torchvision import transforms
+import torchvision
+from tqdm import tqdm
+import lpips
+
+
+import time
+
+start_time = time.time()
+loss_fn_alex = lpips.LPIPS(net='alex') # best forward scores
+loss_fn_vgg = lpips.LPIPS(net='vgg') # closer to "traditional" perceptual loss, when used for optimization
+model_time = time.time()-start_time
+
+img0 = torchvision.io.read_image("/mnt/nas/jiaojiao/data/VOC_new/VOC2012/JPEGImages/2007_000032.jpg")
+img1 = torchvision.io.read_image("/mnt/nas/jiaojiao/data/VOC_new/VOC2012/JPEGImages/2007_000032_new.png")
+io_time = time.time()-start_time
+
+resize_transform = transforms.Resize(( 64, 64))
+# 应用转换
+img0_= resize_transform(img0)
+img1_= resize_transform(img1)
+
+img0_ = torch.reshape(img0_, (1,3,64,64))
+img1_ = torch.reshape(img1_, (1,3,64,64))
+
+d = loss_fn_alex(img0_, img1_)
+
+process_time = time.time()-start_time
+
+print("Perceptual Similarity: ", d.item() )
+print("Time consumptation model time: ", model_time, "io_time: ", io_time-model_time, "process time: ", process_time-io_time )
+
+data_root = "/mnt/nas/jiaojiao/data/VOC_aug/VOC2012/JPEGImages/"
+import os
+filelist = os.listdir(data_root)
+# print(filelist)
+
+def calculate_similarity(img_path1, img_path2):
+    img1 = torchvision.io.read_image(os.path.join(img_path1))
+    img2 = torchvision.io.read_image(os.path.join(img_path2))
+    
+
+    img1_ = resize_transform(img1)
+    img2_ = resize_transform(img2)
+
+    img1_ = torch.reshape(img1_, (1, 3, 64, 64))
+    img2_ = torch.reshape(img2_, (1, 3, 64, 64))
+
+    d = loss_fn_alex(img1_, img2_)
+    
+    return d
+    
+    d_all = []
+    data_size = len(datalist)
+    
+
+    for i, name0 in tqdm(enumerate(datalist)):
+        img0 = torchvision.io.read_image(os.path.join(data_root, name0))
+        for name1 in tqdm(datalist[i+1:]):
+            img1 = torchvision.io.read_image(os.path.join(data_root, name1))
+
+            img0_ = resize_transform(img0)
+            img1_ = resize_transform(img1)
+
+            img0_ = torch.reshape(img0_, (1, 3, 64, 64))
+            img1_ = torch.reshape(img1_, (1, 3, 64, 64))
+
+            d = loss_fn_alex(img0_, img1_)
+
+            d_all.append(d)
+
+    print(len(d_all))
+
+    return sum(d_all) / len(d_all)
+
+# def calculate_inclass_similarity():
+# d = calculate_similarity(filelist)
+# print("Perceptual Similarity: ", d.item() )
